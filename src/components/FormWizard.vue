cb838ed50f2c3d991f920769504039ac678f7165
fsrocha.dev <contato.frankrocha@gmail.com>
2020-02-25 00:47:41 -0300

    optimize step component

--- src/components/FormWizard.vue
+++ src/components/FormWizard.vue
@@ -1,10 +1,6 @@
 <template>
   <div>
-    <FormPlanPicker v-if="currentStepNumber === 1" @update="processStep" />
-    <FormUserDetails v-if="currentStepNumber === 2" @update="processStep" />
-    <FormAddress v-if="currentStepNumber === 3" @update="processStep" :wizard-data="form" />
-    <FormReviewOrder v-if="currentStepNumber === 4" @update="processStep" :wizard-data="form" />
-
+    <component :is="currentStep" @update="processStep" :wizard-data="form"></component>
     <div class="progress-bar">
       <div :style="`width: ${progress}%;`"></div>
     </div>
@@ -14,8 +10,6 @@
       <button @click="goBack" v-if="currentStepNumber > 1" class="btn-outlined">Voltar</button>
       <button @click="goNext" :disabled="!canGoNext" class="btn">Avançar</button>
     </div>
-
-    <pre><code>{{form}}</code></pre>
   </div>
 </template>
 
@@ -36,7 +30,12 @@ export default {
     return {
       currentStepNumber: 1,
       canGoNext: false,
-      length: 4,
+      steps: [
+        "FormPlanPicker",
+        "FormUserDetails",
+        "FormAddress",
+        "FormReviewOrder"
+      ],
       form: {
         plan: null,
         email: null,
@@ -50,6 +49,12 @@ export default {
     };
   },
   computed: {
+    currentStep() {
+      return this.steps[this.currentStepNumber - 1];
+    },
+    length() {
+      return this.steps.length;
+    },
     progress() {
       return (this.currentStepNumber / this.length) * 100;
     }
